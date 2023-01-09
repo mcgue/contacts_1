6585b769430c67d6c626d3aa5f9e2e701e84a46e
mcgue <mcgu@protonmail.com>
2023-01-08 20:45:05 -0800

    add print function

--- main.py
+++ main.py
@@ -9,36 +9,22 @@ def save_contact(contact: dict, filename: str):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("This program saves a Contact in your Contacts List")
-    name = input("Contact name? ")
-    email = input("Contact email? ")
-    phone = input("Contact phone? ")
-    relationship = input("Contact relationship? ")
+    # print("This program saves a Contact in your Contacts List")
+    # name = input("Contact name? ")
+    # email = input("Contact email? ")
+    # phone = input("Contact phone? ")
+    # relationship = input("Contact relationship? ")
 
-    contact = {
-        "name": name,
-        "email": email,
-        "phone": phone,
-        "relationship": relationship
-    }
+    #contact = {
+    #   "name": name,
+    #   "email": email,
+    #   "phone": phone,
+    #   "relationship": relationship
+    #}
 
-    save_contact(contact, "contacts.json")
-
-    # Print contacts
     # Opening JSON file
-    f = open("contacts.json")
+    with open('contacts.json') as user_file:
+        file_contents = user_file.read()
 
-    # returns JSON object as a dictionary
-    data_json = json.load("contacts.json")
-
-    # Create and print
-    #json_formatted_str = json.dumps(data_json, indent=2)
-
-    #print(json_formatted_str)
-
-    # Iterating through the json list to print
-    #for i in data_json["name"]:
-    #    print(i)
-
-    # Closing file
-    #f.close()
+    # Print all contacts
+    print(file_contents)
