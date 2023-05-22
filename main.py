e6b8d3b3ba3ade622f98fc40bd5be334b1c3590a
mcgue <mcgu@protonmail.com>
2023-05-22 11:21:59 -0700

    resume editing

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Not finished
 # Create Contacts List
 # based on https://pythonalgos.com/super-simple-python-contacts-list/
 
@@ -10,11 +9,13 @@ def save_contact(contact: dict, filename: str):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # print("This program saves a Contact in your Contacts List")
-    # name = input("Contact name? ")
-    # email = input("Contact email? ")
-    # phone = input("Contact phone? ")
-    # relationship = input("Contact relationship? ")
+    # Introduction
+    print("This program saves a Contact in your Contacts List")
+    # Get data
+    name = input("Contact name? ")
+    email = input("Contact email? ")
+    phone = input("Contact phone? ")
+    relationship = input("Contact relationship? ")
 
     #contact = {
     #   "name": name,
@@ -23,8 +24,3 @@ if __name__ == '__main__':
     #   "relationship": relationship
     #}
 
-    # Opening JSON file
-    with open('contacts.json') as json_file:
-        data = json.load(json_file)
-
- 
