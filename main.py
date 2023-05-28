9a2c6f6bd1bd181a152819850f85a4cb506c44f7
mcgue <mcgu@protonmail.com>
2023-05-28 14:19:58 -0700

    finish save function by putting most code in it

--- main.py
+++ main.py
@@ -3,27 +3,27 @@
 
 import json
 
-def save_contact(contact: dict, filename: str):
-    with open(filename, "a") as f:
-        json.dump(contact, f)
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    # Introduction
-    print("This program saves a Contact in your Contacts List")
-    # Declare file location
-    filename = 'contacts.json'
+def save_contact(filename: str):
     # Get data
     name = input("Contact name? ")
     email = input("Contact email? ")
     phone = input("Contact phone? ")
     relationship = input("Contact relationship? ")
-
+    # Link responses to a format to add to json
     contact = {
-       "name": name,
-       "email": email,
-       "phone": phone,
-       "relationship": relationship
+        "name": name,
+        "email": email,
+        "phone": phone,
+        "relationship": relationship
     }
+    # Add contact to json file
+    with open(filename, "a") as f:
+        json.dump(contact, f)
 
-    save_contact(contact, filename)
+if __name__ == '__main__':
+    # Introduction
+    print("This program saves a Contact in your Contacts List")
+    # Declare file location
+    filename = 'contacts.json'
+    # Run function to get new contact data and save it to json file
+    save_contact(filename)
